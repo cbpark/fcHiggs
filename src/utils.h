6f0f4332e6b79bf6b7696cd7744408a6f26dcff2
cbpark <cbpark@gmail.com>
2017-08-31 16:15:01 +0900

    Add more util functions

--- src/utils.h
+++ src/utils.h
@@ -16,6 +16,13 @@
 namespace fchiggs {
 double getRandom();
 
+inline double lambda12(const double x, const double y, const double z) {
+    double lambda = x * x + y * y + z * z - 2 * x * y - 2 * y * z - 2 * z * x;
+    return std::sqrt(lambda);
+}
+
+inline double costh(const double delta) { return -1.0 + getRandom() * delta; }
+
 inline std::pair<double, double> sigma(const double sum_w,
                                        const double sum_w_sq,
                                        const unsigned int n) {
