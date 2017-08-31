942337d0a0c9e0f9c4be880d2bd96d12d9be8768
cbpark <cbpark@gmail.com>
2017-08-31 17:32:11 +0900

    Add kinematics classes and functions

---
+++ src/kinematics.cc
@@ -0,0 +1,72 @@
+/*
+ *  Copyright (C) 2017 Chan Beom Park <cbpark@gmail.com>
+ *
+ *  This file is part of fcHiggs, which is released under the GNU General
+ *  Public License. See file LICENSE in the top directory of this project
+ *  or go to <http://www.gnu.org/licenses/> for full license details.
+ */
+
+#include "kinematics.h"
+#include <cmath>
+#include <ostream>
+#include "constants.h"
+#include "utils.h"
+
+namespace fchiggs {
+std::ostream &operator<<(std::ostream &os, const FourMomentum &p) {
+    os << "e = " << p.e_ << ", px = " << p.px_ << ", py = " << p.py_
+       << ", pz = " << p.pz_;
+    return os;
+}
+
+FourMomentum boostZ(const FourMomentum &p, const double beta) {
+    const double gamma = 1.0 / std::sqrt(1 - beta * beta);
+    const double gb = gamma * beta;
+    return {gamma * p.e_ - gb * p.pz_, p.px_, p.py_,
+            -gb * p.e_ + gamma * p.pz_};
+}
+
+double phi_mpi_pi(double phi) {
+    while (phi >= PI) { phi -= TWOPI; }
+    while (phi < -PI) { phi += TWOPI; }
+    return phi;
+}
+
+double deltaPhi(const FourMomentum &p1, const FourMomentum &p2) {
+    return phi_mpi_pi(p1.phi() - p2.phi());
+}
+
+double deltaR(const FourMomentum &p1, const FourMomentum &p2) {
+    const double deta = p1.eta() - p2.eta();
+    const double dphi = deltaPhi(p1, p2);
+    return std::sqrt(deta * deta + dphi * dphi);
+}
+
+void CM22::init() {
+    const double e = 2.0 * std::sqrt(s_);
+    pin_ = (s_ - md2_) / e;
+    pfin_ = lambda12(s_, mh2_, MB2) / e;
+}
+
+FourMomentum CM22::p1() const {
+    const double energy = (s_ + md2_) / (2.0 * std::sqrt(s_));
+    return {energy, 0, 0, pin_};
+}
+
+FourMomentum CM22::k1() const {
+    const double energy = (s_ - md2_) / (2.0 * std::sqrt(s_));
+    return {energy, 0, 0, -pin_};
+}
+
+FourMomentum CM22::p2() const {
+    const double energy = (s_ - mh2_ + MB2) / (2.0 * std::sqrt(s_));
+    return {energy, -pfin_ * sinth_ * std::cos(phi_),
+            -pfin_ * sinth_ * std::sin(phi_), -pfin_ * costh_};
+}
+
+FourMomentum CM22::k2() const {
+    const double energy = (s_ + mh2_ - MB2) / (2.0 * std::sqrt(s_));
+    return {energy, pfin_ * sinth_ * std::cos(phi_),
+            pfin_ * sinth_ * std::sin(phi_), pfin_ * costh_};
+}
+}  // namespace fchiggs
---
+++ src/kinematics.h
@@ -0,0 +1,99 @@
+/*
+ *  Copyright (C) 2017 Chan Beom Park <cbpark@gmail.com>
+ *
+ *  This file is part of fcHiggs, which is released under the GNU General
+ *  Public License. See file LICENSE in the top directory of this project
+ *  or go to <http://www.gnu.org/licenses/> for full license details.
+ */
+
+#ifndef FCHIGGS_SRC_KINEMATICS_H_
+#define FCHIGGS_SRC_KINEMATICS_H_
+
+#include <cmath>
+#include <ostream>
+#include "constants.h"
+#include "utils.h"
+
+namespace fchiggs {
+class FourMomentum {
+private:
+    double e_, px_, py_, pz_;
+
+public:
+    FourMomentum() = delete;
+    FourMomentum(const double e, const double px, const double py,
+                 const double pz)
+        : e_{e}, px_{px}, py_{py}, pz_{pz} {}
+
+    double m2() const { return e_ * e_ - px_ * px_ - py_ * py_ - pz_ * pz_; }
+
+    double phi() const {
+        return px_ == 0 && py_ == 0 ? 0 : std::atan2(py_, px_);
+    }
+
+    double eta() const { return 0.5 * log((e_ + pz_) / (e_ - pz_)); }
+
+    FourMomentum operator-() const { return {-e_, -px_, -py_, -pz_}; }
+
+    friend FourMomentum operator+(FourMomentum lhs, const FourMomentum &rhs) {
+        lhs.e_ += rhs.e_;
+        lhs.px_ += rhs.px_;
+        lhs.py_ += rhs.py_;
+        lhs.pz_ += rhs.pz_;
+        return lhs;
+    }
+
+    friend FourMomentum operator-(FourMomentum lhs, const FourMomentum &rhs) {
+        lhs.e_ -= rhs.e_;
+        lhs.px_ -= rhs.px_;
+        lhs.py_ -= rhs.py_;
+        lhs.pz_ -= rhs.pz_;
+        return lhs;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const FourMomentum &p);
+
+    friend FourMomentum boostZ(const FourMomentum &p, const double beta);
+};
+
+FourMomentum boostZ(const FourMomentum &p, const double beta);
+
+double deltaPhi(const FourMomentum &p1, const FourMomentum &p2);
+
+double deltaR(const FourMomentum &p1, const FourMomentum &p2);
+
+/**
+ *  Particle momenta of 2 --> 2 process at the CM frame.
+ *
+ *  d(p1) g(k1) --> b(p2) H(k2)
+ */
+class CM22 {
+private:
+    double s_, mh2_, md2_;
+    double costh_, sinth_, phi_;
+    double pin_, pfin_;
+
+public:
+    CM22() = delete;
+    CM22(const double s, const double mh, const double md)
+        : s_(s),
+          mh2_(mh * mh),
+          md2_(md * md),
+          costh_(costh(DELTA)),
+          sinth_(std::sqrt(1.0 - costh_ * costh_)),
+          phi_(TWOPI * getRandom()) {}
+
+    FourMomentum p1() const;
+    FourMomentum k1() const;
+    FourMomentum p2() const;
+    FourMomentum k2() const;
+
+    double s_hat() const { return s_; }
+    double t_hat() const { return (p1() - k2()).m2(); }
+
+private:
+    void init();
+};
+}  // namespace fchiggs
+
+#endif  // FCHIGGS_SRC_KINEMATICS_H_
